a67e76bd8cfc91a728e158eac2672a241f8c4195
ashfaq-polit <ashfaq.polit@gmail.com>
2020-10-04 08:19:00 -0400

    Update Assignment_4.py

--- Assignment_4.py
+++ Assignment_4.py
@@ -33,26 +33,19 @@ from scipy.stats import ttest_ind
 # 
 # Each function in this assignment below is worth 10%, with the exception of ```run_ttest()```, which is worth 50%.
 
-# In[ ]:
-
-
-
-
-
-# In[2]:
 
 
 def get_list_of_university_towns():
-    '''Returns a DataFrame of towns and the states they are in from the 
-    university_towns.txt list. The format of the DataFrame should be:
-    DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ], 
-    columns=["State", "RegionName"]  )
+    #'''Returns a DataFrame of towns and the states they are in from the 
+    #university_towns.txt list. The format of the DataFrame should be:
+    #DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ], 
+    #columns=["State", "RegionName"]  )
     
-    The following cleaning needs to be done:
+    #The following cleaning needs to be done:
 
-    1. For "State", removing characters from "[" to the end.
-    2. For "RegionName", when applicable, removing every character from " (" to the end.
-    3. Depending on how you read the data, you may need to remove newline character '\n'. '''
+    #1. For "State", removing characters from "[" to the end.
+    #2. For "RegionName", when applicable, removing every character from " (" to the end.
+    #3. Depending on how you read the data, you may need to remove newline character '\n'. '''
     
     import pandas as pd
     import numpy as np
@@ -101,24 +94,18 @@ def get_list_of_university_towns():
     return df
 
 
-# In[3]:
-
-
 def get_recession_start():
-    '''Returns the year and quarter of the recession start time as a 
-    string value in a format such as 2005q3'''
+#'''Returns the year and quarter of the recession start time as a 
+#string value in a format such as 2005q3'''
     recession_start = df1[(df1['Difference'].shift(1)>0) & (df1['Difference']<0) & (df1['Difference'].shift(-1)<0)]
     recession_start = recession_start.dropna()   
     return recession_start['Quarter']
-    
 
-
-# In[4]:
 
 
 def get_recession_end():
-    '''Returns the year and quarter of the recession end time as a 
-    string value in a format such as 2005q3'''
+#'''Returns the year and quarter of the recession end time as a 
+#   string value in a format such as 2005q3'''
     recession_end = df1[(df1['Difference'].shift(1)<0) & (df1['Difference'].shift(2)<0) 
                 & (df1['Difference']>0) & (df1['Difference'].shift(-1)>0)]
     recession_end = recession_end.dropna()      
@@ -126,12 +113,9 @@ def get_recession_end():
     
 
 
-# In[5]:
-
-
 def get_recession_bottom():
-    '''Returns the year and quarter of the recession bottom time as a 
-    string value in a format such as 2005q3'''
+#'''Returns the year and quarter of the recession bottom time as a 
+#   string value in a format such as 2005q3'''
     
     recession_period = df1.loc[recession_start.index[0]:recession_end.index[0],:]
     recession_min = recession_period['Difference'].idxmin()
@@ -139,20 +123,18 @@ def get_recession_bottom():
     return recession_bottom
 
 
-# In[6]:
-
 
 def convert_housing_data_to_quarters():
-    '''Converts the housing data to quarters and returns it as mean 
-    values in a dataframe. This dataframe should be a dataframe with
-    columns for 2000q1 through 2016q3, and should have a multi-index
-    in the shape of ["State","RegionName"].
+   # '''Converts the housing data to quarters and returns it as mean 
+   #values in a dataframe. This dataframe should be a dataframe with
+   #columns for 2000q1 through 2016q3, and should have a multi-index
+   # in the shape of ["State","RegionName"].
     
-    Note: Quarters are defined in the assignment description, they are
-    not arbitrary three month periods.
+   #Note: Quarters are defined in the assignment description, they are
+   # not arbitrary three month periods.
     
-    The resulting dataframe should have 67 columns, and 10,730 rows.
-    '''
+   # The resulting dataframe should have 67 columns, and 10,730 rows.
+    
     
     
     df2 = pd.read_csv('City_house_price.csv')
@@ -171,23 +153,21 @@ def convert_housing_data_to_quarters():
     return df2_up
 
 
-# In[7]:
-
 
 def run_ttest():
-    '''First creates new data showing the decline or growth of housing prices
-    between the recession start and the recession bottom. Then runs a ttest
-    comparing the university town values to the non-university towns values, 
-    return whether the alternative hypothesis (that the two groups are the same)
-    is true or not as well as the p-value of the confidence. 
+    #'''First creates new data showing the decline or growth of housing prices
+    #between the recession start and the recession bottom. Then runs a ttest
+    #comparing the university town values to the non-university towns values, 
+    #return whether the alternative hypothesis (that the two groups are the same)
+    #is true or not as well as the p-value of the confidence. 
     
-    Return the tuple (different, p, better) where different=True if the t-test is
-    True at a p<0.01 (we reject the null hypothesis), or different=False if 
-    otherwise (we cannot reject the null hypothesis). The variable p should
-    be equal to the exact p value returned from scipy.stats.ttest_ind(). The
-    value for better should be either "university town" or "non-university town"
-    depending on which has a lower mean price ratio (which is equivilent to a
-    reduced market loss).'''
+    #Return the tuple (different, p, better) where different=True if the t-test is
+    #True at a p<0.01 (we reject the null hypothesis), or different=False if 
+    #otherwise (we cannot reject the null hypothesis). The variable p should
+    #be equal to the exact p value returned from scipy.stats.ttest_ind(). The
+    #value for better should be either "university town" or "non-university town"
+    #depending on which has a lower mean price ratio (which is equivilent to a
+    #reduced market loss).'''
     
     df3=df.copy(deep=True)
     states_new = {v: k for k, v in states.items()}
@@ -217,9 +197,6 @@ def run_ttest():
         x = min(uni_recession['price_ratio'].mean,non_uni_recession['price_ratio'].mean)
     else: x = None
     return stat,p,x
-
-
-# In[ ]:
 
 
 
